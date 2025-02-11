f4e79c3adc93330a307a33bdc740a38398694a6e
JulienGln <juliengalerne.jg@gmail.com>
2025-02-11 14:13:32 +0100

    utilisation de la v2
    Co-Authored-By: Théo Matrod <75441296+C4RBON73@users.noreply.github.com>
    Co-Authored-By: Gor G. <99032002+gor-g@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import gradio as gr
 from huggingface_hub import InferenceClient, login
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
 # exemple : https://github.com/blancsw/deep_4_all/blob/main/tgi_demo/app.py
 
 login("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")
 
 
-client = InferenceClient("https://api-inference.huggingface.co/models/Gor-bepis/fact-checker-bfmtg-v1")
+client = InferenceClient()
 
 
 def chat(input: str, history: list[tuple[str, str]]):
@@ -22,7 +23,7 @@ def chat(input: str, history: list[tuple[str, str]]):
     messages.append({"role": "user", "content": input})
 
     chat_completion = client.chat.completions.create(
-        model="Gor-bepis/fact-checker-bfmtg-v1",
+        model="Gor-bepis/fact-checker-bfmtg-v2",
         stream=True,
         messages=messages,
         max_tokens=1024
@@ -38,8 +39,8 @@ def chat(input: str, history: list[tuple[str, str]]):
 
     # return resp
 
-checkpoint = "Gor-bepis/fact-checker-bfmtg-v1"
-device = "cuda"
+checkpoint = "Gor-bepis/fact-checker-bfmtg-v2"
+device = "cuda" if torch.cuda.is_available() else "cpu"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
 
@@ -60,8 +61,9 @@ with gr.Blocks(theme=gr.themes.Soft(primary_hue="blue", secondary_hue="teal"), t
         description="Bienvenue sur le chatbot qui vous dira si ce que vous avancez est vrai ou faux.",
         show_progress="full",
         examples=[
-            "Is it true that the earth is flat ?", 
-            "I've heard that the release of GTA VI will be postponed. I hope it's false, isn't it ?"
+            "TUCKER CARLSON Tells Liberal Guest: National Endowment for the Arts Is In Effect ‘Welfare for Rich, Liberal Elites’ [VIDEO] Tucker Carlson asks Robin Bronk, CEO of the Creative Coalition: Why should in a time of budge deficits, taxpayers be subsidizing entertainment for rich people ? Don t you think it s kind of funny that artists who are against the grain and thinking for themselves, all of a sudden they re queuing up for their handouts from taxpayers? Tucker asked. Why wouldn t artists just strike out on their own and be independent? Watch the back-and-forth here: FOX Insider",
+            "Trump arrives in South Korea for talks on nukes, trade OSAN, South Korea (Reuters) - U.S. President Donald Trump landed in South Korea on Tuesday, the second leg of his 12-day Asia trip dominated by the North Korean nuclear standoff. South Koreans are bracing for the possibility that Trump s state visit could risk further inflaming tensions with North Korean leader Kim Jong Un, who has stepped up his pursuit of nuclear weapons that could soon be capable of striking the mainland United States. Trump will visit with U.S troops and is also expected to raise criticisms of a U.S-South Korean trade pact when he meets with President Moon Jae-in in Seoul.",
+            "AWESOME! STREET ARTIST SABO TARGETS Hollywood Liberals With TRUMP ’24’ Spoof Posters Protesters gathered Friday in Los Angeles for something called United Against Hate Inauguration March, where street artist Sabo posted several fake advertisements for 24: Legacy. Sabo is our favorite conservative street artist famous for lampooning liberal Hollywood. He s celebrated the inauguration of Trump at an anti-Trump rally Friday in Los Angeles by posting faux posters of Fox s revival of the hit TV show 24:Read more: Hollywood Reporter"
         ]
     )
 
